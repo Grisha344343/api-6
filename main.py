562e5596becaa8847e8a6416cb7f2360420efa29
Grisha344343 <100464013+Grisha344343@users.noreply.github.com>
2023-06-03 12:07:31 +0300

    скачал комикс про питон

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import requests
+import pprint
 
+filename = "comic.png"
+url = "https://xkcd.com/353/info.0.json"
+response = requests.get(url)
+response.raise_for_status()
+comic_info = response.json()
+comic_url = comic_info["img"]
+response = requests.get(comic_url)
+response.raise_for_status()
 
-
-dwdwdwdwd
-
-
-
-
-if __name__ == '__main__':
+with open(filename, 'wb') as file:
+    file.write(response.content)
 
